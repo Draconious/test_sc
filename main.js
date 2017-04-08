960d5881bb0e59136c26d9b1c91a1d7366a298f2
SeanG <sean@gsd.co.za>
2017-04-08 16:48:07 +0100

    Testing fix for extra builders

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
-var roleRepairer = require('role.repairer');
-var roleTransferer = require('role.transferer');
+var taskWork = require('task.work');
+var taskPopulate = require('task.populate');
 
 module.exports.loop = function () {
     for(var i in Memory.creeps) {
@@ -32,67 +29,11 @@ module.exports.loop = function () {
         }
     }
 
-    var harvesters = [];
-    var builders = [];
-    var upgraders = [];
-    var repairers = [];
-    var transferers = [];
-
-    if (!Game.spawns.Spawn1.spawning && Game.spawns.Spawn1.energy === 300) {
-
-        for(var i in Game.creeps) {
-            if (Game.creeps[i].memory.role === 'harvester') {
-                harvesters.push(Game.creeps[i]);
-            }
-            if (Game.creeps[i].memory.role === 'upgrader') {
-                upgraders.push(Game.creeps[i]);
-            }
-            if (Game.creeps[i].memory.role === 'builder') {
-                builders.push(Game.creeps[i]);
-            }
-            if (Game.creeps[i].memory.role === 'repairer') {
-                repairers.push(Game.creeps[i]);
-            }
-            if (Game.creeps[i].memory.role === 'transferer') {
-                transferers.push(Game.creeps[i]);
-            }
-        }
-
-        if(harvesters.length < 3) {
-            console.log("Create harvester, source " + (harvesters.length > 0 && harvesters[harvesters.length - 1].memory.source === 0 ? 1 : 0));
-            Game.spawns.Spawn1.createCreep([WORK, CARRY, CARRY, MOVE, MOVE], null, {role: 'harvester', source: (harvesters.length > 0 && harvesters[harvesters.length - 1].memory.source === 0 ? 1 : 0)});
-        } else if (upgraders.length < 2) {
-            console.log("Create upgrader, source " + (upgraders.length > 0 && upgraders[upgraders.length - 1].memory.source === 0 ? 1 : 0));
-            //Game.spawns.Spawn1.createCreep([WORK, CARRY, CARRY, MOVE, MOVE], null, {role: 'upgrader', source: (upgraders.length > 0 && upgraders[upgraders.length - 1].memory.source === 0 ? 1 : 0)});
-            Game.spawns.Spawn1.createCreep([WORK, CARRY, CARRY, MOVE, MOVE], null, {role: 'upgrader', source: 1});
-        } else if (builders.length < 2) {
-            console.log("Create builder, source " + (builders.length > 0 && builders[builders.length - 1].memory.source === 0 ? 1 : 0));
-            Game.spawns.Spawn1.createCreep([WORK, CARRY, CARRY, MOVE, MOVE], null, {role: 'builder', source: (builders.length > 0 && builders[builders.length - 1].memory.source === 0 ? 1 : 0)});
-        } else if (repairers.length < 1) {
-            console.log("Create repairer, source " + (repairers.length > 0 && repairers[repairers.length - 1].memory.source === 0 ? 1 : 0));
-            Game.spawns.Spawn1.createCreep([WORK, CARRY, CARRY, MOVE, MOVE], null, {role: 'repairer', source: (repairers.length > 0 && repairers[repairers.length - 1].memory.source === 0 ? 1 : 0)});
-        } else if (transferers.length < 1) {
-            console.log("Create transferer, source " + (transferers.length > 0 && transferers[transferers.length - 1].memory.source === 0 ? 1 : 0));
-            Game.spawns.Spawn1.createCreep([WORK, CARRY, CARRY, MOVE, MOVE], null, {role: 'transferer', source: (transferers.length > 0 && transferers[transferers.length - 1].memory.source === 0 ? 1 : 0)});
-        }
+    if (!Game.spawns.Spawn1.spawning && Game.spawns.Spawn1.energy >= 300) {
+        taskPopulate.run();
     }
 
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        if(creep.memory.role === 'harvester') {
-            roleHarvester.run(creep);
-        }
-        if(creep.memory.role === 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-        if(creep.memory.role === 'builder') {
-            roleBuilder.run(creep);
-        }
-        if(creep.memory.role === 'repairer') {
-            roleRepairer.run(creep);
-        }
-        if(creep.memory.role === 'transferer') {
-            roleTransferer.run(creep);
-        }
-    }
+    taskWork.run();
+
+
 };
