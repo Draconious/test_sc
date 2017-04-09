5b4b9cc7a370531898ad5b196feed86afc60da98
SeanG <sean@gsd.co.za>
2017-04-09 12:06:14 +0100

    Fixed spelling

--- task.populate.js
+++ task.populate.js
@@ -12,11 +12,11 @@ var taskPopulate = {
 
         var repairerSource = 0;
         var repairerCount = 0;
-        var maxuRepairerCount = 3;
+        var maxRepairerCount = 3;
 
         var transfererSource = 0;
         var transfererCount = 0;
-        var maxuTransfererCount = 3;
+        var maxTransfererCount = 3;
 
         for(var i in Game.creeps) {
             if (Game.creeps[i].memory.role === 'harvester') {
@@ -41,12 +41,12 @@ var taskPopulate = {
             console.log("Create harvester " + harvesterCount + ", source " + (harvesterSource < Math.ceil(maxHarvesterCount / 2) ? 1 : 0));
             Game.spawns.Spawn1.createCreep([WORK, WORK, CARRY, CARRY, MOVE], null, {role: 'harvester', source: (harvesterSource < 3 ? 1 : 0)});
         } else if (upgraderCount < maxUpgraderCount) {
-            console.log("Create upgrader " + upgraderCount + ", source " + (upgraderSource < Math.ceil(maxUpgraderCount / 2) ? 1 : 0));
-            Game.spawns.Spawn1.createCreep([WORK, CARRY, CARRY, MOVE, MOVE], null, {role: 'upgrader', source: (upgraderSource < 3 ? 1 : 0)});
-        } else if (repairerCount < maxuRepairerCount) {
+            console.log("Create upgrader " + upgraderCount + ", source " + 1);
+            Game.spawns.Spawn1.createCreep([WORK, CARRY, CARRY, MOVE, MOVE], null, {role: 'upgrader', source: 1});
+        } else if (repairerCount < maxRepairerCount) {
             console.log("Create repairer " + repairerCount + ", source " + (repairerSource < Math.ceil(maxRepairerCount / 2) ? 1 : 0));
             Game.spawns.Spawn1.createCreep([WORK, CARRY, CARRY, MOVE, MOVE], null, {role: 'repairer', source: (repairerSource < 3 ? 1 : 0)});
-        } else if (transfererCount < maxuTransfererCount) {
+        } else if (transfererCount < maxTransfererCount) {
             console.log("Create transferer " + transfererCount + ", source " + 0);
             Game.spawns.Spawn1.createCreep([CARRY, CARRY, MOVE, MOVE], null, {role: 'transferer', source: 0});
         }
